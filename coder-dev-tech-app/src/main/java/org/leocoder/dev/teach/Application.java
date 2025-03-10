096a21a3a91ce7997060b9f8b89323416458fd5c
leocoder <2942894660@qq.com>
2025-03-10 13:45:14 +0800

    feat：补充启动问题

--- coder-dev-tech-app/src/main/java/org/leocoder/dev/teach/Application.java
+++ coder-dev-tech-app/src/main/java/org/leocoder/dev/teach/Application.java
@@ -2,7 +2,9 @@ package org.leocoder.dev.teach;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author : 程序员Leo
@@ -13,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Configurable
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
